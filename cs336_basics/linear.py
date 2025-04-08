e8ceab86d44f0757146823fc3a86b48f6576c65d
Radi Cho <radicho123@gmail.com>
2025-04-07 18:25:49 -0700

    Add typing to linear.

--- cs336_basics/linear.py
+++ cs336_basics/linear.py
@@ -1,14 +1,15 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 
 class Linear(nn.Module):
-    def __init__(self, in_features, out_features, device=None, dtype=None):
+    def __init__(self, in_features: int, out_features: int, device: str | None = None, dtype: torch.dtype | None = None) -> None:
         super().__init__()
         kwargs = {'device': device, 'dtype': dtype}
 
-        self.weight = nn.Parameter(torch.empty(in_features, out_features, **kwargs))
-        std = 2/(in_features + out_features)
+        self.weight: nn.Parameter = nn.Parameter(torch.empty(in_features, out_features, **kwargs))
+        std = 2 / (in_features + out_features)
         torch.nn.init.trunc_normal_(self.weight, 0, std, -3 * std, 3 * std)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         return x @ self.weight.T
